b0b3e25be0cc2a2981273944b259625388e7865d
Julen37 <julen.gouchault@gmail.com>
2025-12-15 11:06:23 +0100

    fix bug token backend

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ const PORT = process.env.PORT || 3000;
 
 // definition du middleware pour parser les requetes https en json
 job.start();
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '10mb' })); // parse les requêtes JSON avec une limite de taille de 10 Mo
+app.use(express.urlencoded({ extended: true, limit: '10mb'})); //parse es requetes htpp avec des données encodées en URL
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
